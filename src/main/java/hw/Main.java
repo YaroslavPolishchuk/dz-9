2f9317cb47bce8a4cba9e45411c26a384822cca0
Yaroslav <aim4rip@gmail.com>
2023-05-09 18:23:48 +0300

    start

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,7 +1,42 @@
 package hw;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void printRetire(boolean isRe) {
+        if(isRe){
+            System.out.println("Ya uge vsjo, na pensii)");
+        }
+        else {
+            System.out.println("I am like a horse");
+        }
+
     }
+
+    public static void main(String[] args) {
+        Woman lesya = new Woman("Lesya", "Lesina", 38);
+        Woman zina = new Woman("Zina", "Z", 61);
+
+        printRetire(lesya.isRetired());
+        printRetire(zina.isRetired());
+
+        Man vitya = new Man("Vitya","Tsoi",39);
+        Man vanya = new Man("Vanya","Garbuz",63);
+
+        System.out.println(vitya.getLastName());
+        System.out.println(lesya.getLastName());
+        lesya.registerPartnership(vitya);
+        vitya.registerPartnership(lesya);
+        System.out.println(lesya.getLastName());
+        System.out.println(vitya.getLastName());
+
+        vitya.getPartnerInfo();
+
+        lesya.deRegisterPartnersip();
+        System.out.println(lesya.getLastName());
+        lesya.getPartnerInfo();
+
+
+        printRetire(vanya.isRetired());
+
+    }
+
 }
--- src/main/java/hw/Person.java
+++ src/main/java/hw/Person.java
@@ -6,10 +6,15 @@ public class Person {
     int age;
     Person partner;
     String maiden;
+
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public int getAge() {
@@ -19,16 +24,26 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
-    public boolean isRetired(){
-       return getAge()>=65;
-    }
-    public void registerPartnership(Person partner ){
-        maiden=lastName;
-        lastName=partner.lastName;
-    }
-    public  void deRegisterPartnersip(){
-        lastName=maiden;
+
+    public boolean isRetired() {
+        return getAge() >= 65;
     }
 
+    public void registerPartnership(Person partner) {
+        maiden = lastName;
+        lastName = partner.lastName;
+        this.partner = partner;
+    }
 
+    public void deRegisterPartnersip() {
+        if (!maiden.isEmpty())
+            lastName = maiden;
+        partner = null;
+    }
+    public void getPartnerInfo(){
+        if(partner!=null)
+            System.out.println(String.format("%s %s \n %s",partner.lastName,partner.firstName,partner.getAge()));
+        else
+            System.out.println(String.format("%s doesn't has partner",firstName));
+    }
 }
