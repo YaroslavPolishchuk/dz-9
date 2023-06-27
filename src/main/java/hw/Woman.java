c91cba77e86d694c4fd728e5d0758e7ff6bad62f
Yaroslav <aim4rip@gmail.com>
2023-06-27 10:34:36 +0300

    tests

--- src/main/java/hw/Woman.java
+++ src/main/java/hw/Woman.java
@@ -4,6 +4,9 @@ public class Woman extends Person{
     public Woman(String firstName, String lastName, int age) {
         super(firstName, lastName, age);
     }
+    public Woman() {
+        super();
+    }
 
     @Override
     public boolean isRetired() {
